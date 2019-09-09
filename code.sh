28bd44f0789c8d5defa2f4446071ae1cd5ffc9d1
arraguman <raguraman.j@prodapt.com>
2019-09-09 15:22:47 +0530

    intial commit

---
+++ code.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+prev_dir=/home/devops5/OldDirectory
+new_dir=/home/devops5/NewDirectory
+cd $prev_dir
+#FILES=du -h /var | sort -n -r | head -n 5
+for i in *.txt
+do
+   #sed -i 's/\r$//' $i 
+   echo $i
+   cd $prev_dir
+   mv $i $new_dir
+done
